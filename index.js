015b723caacdd3f649b3163c9bf1cc1f6fa8cab5
Gp3ShenQ <gp3g4xup6@gmail.com>
2023-10-24 23:10:52 +0800

    v1.6

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function getTime(){
   let minute = String(time.getMinutes()).padStart(2, "0")
   $('.month').text(`${month}月${date}日`)
   $('.hour').text(`${hour}:${minute}`)
-  
 }
 
 function updateTime(){
@@ -18,37 +17,48 @@ function updateTime(){
 getTime()
 updateTime()
 
-$(".button1").on("click",function(){
-  $(".button1").removeClass("buttonStyle")
-  $(".button1").addClass("buttonStyleIn")
-  $(".button2,.button3,.button4").removeClass("buttonStyleIn")
-  $(".button2,.button3,.button4").addClass("buttonStyle")
-  $(".indexBox").removeClass("animation")
-})
 $(".button2").on("click",function(){
-  $(".button2").toggleClass("buttonStyleIn")
+  $(this).toggleClass("press")
   if ($(".indexBox").hasClass("animation")){
     $(".indexBox").removeClass("animation")
     return
   }
   $(".indexBox").addClass("animation")
-  $(".button1,.button3,.button4").removeClass("buttonStyleIn")
-  $(".button1,.button3,.button4").addClass("buttonStyle")
 })
 
+$(".button1").on("click",function(){
+  $(this).toggleClass("press")
+  if ($(".mes-area").hasClass("openAnimation")){
+    $(".mes-area").removeClass("openAnimation")
+    $(".mes-area").addClass("closeAnimation")
+    return
+  }
+  $(".mes-area").addClass("openAnimation")
+  })
 
-$(".button3").on("click",function(){
-  $(".button3").removeClass("buttonStyle")
-  $(".button3").addClass("buttonStyleIn")
-  $(".button2,.button1,.button4").removeClass("buttonStyleIn")
-  $(".button2,.button1,.button4").addClass("buttonStyle")
-  $(".indexBox").removeClass("animation")
-})
-$(".button4").on("click",function(){
-  $(".button4").removeClass("buttonStyle")
-  $(".button4").addClass("buttonStyleIn")
-  $(".button2,.button3,.button1").removeClass("buttonStyleIn")
-  $(".button2,.button3,.button1").addClass("buttonStyle")
-  $(".indexBox").removeClass("animation")
-})
+const messageControl = {
+  template: `<div class="message">
+    <p class="title">{{title}}</p>
+    <p class="inner-text">{{text}}</p>
+  </div>`,
+  addMessage(){
+    console.log('submit')
+    const title = $('.mes-title input').val();
+    const text = $('.mes-text input').val();
+    $('.messageBox').append(this.template.replace('{{title}}',title).replace('{{text}}',text))
+    $('.mes-title input').val('')
+    $('.mes-text input').val('')
+  },
+  submitEvent(){
+    const self = this;
+    $('.output').on('click',function(){
+      self.addMessage()
+    })
+  },
+  init(){
+    this.submitEvent()
+  }
+}
+
+messageControl.init()
 
